de7a5469c714693d8c96b48d4ddaa616b5d740d9
Run Lertjitthamrong <run.lertjit@mail.kmutt.ac.th>
2022-02-19 22:26:36 +0700

    Add grid

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import {ref,reactive} from 'vue'
+  import {ref,reactive, computed} from 'vue'
   import {word} from './word.json'
   
   const evalueteStatus = ["present","correct","absent"]
@@ -9,7 +9,8 @@
 
   const randomWord = word1[Math.floor(Math.random()*2315)]
   const inputWord = ref('')
-  const round = 0
+  const round = ref(0)
+  const words = ref([]);
   const board = reactive([{
     bordState : '',
     evalution : []
@@ -36,24 +37,25 @@
     }])
   console.log(randomWord);
   const checkInput = () => {
-    if(round==6){return console.log('You Lose');}
+    if(round.value==6){return console.log('You Lose');}
     if(randomWord==inputWord.value.toLowerCase()){
-      return console.log(`round:${round} You WIN`);
+      return console.log(`round:${round.value} You WIN`);
     }
     if(word1.includes(inputWord.value.toLowerCase())||word2.includes(inputWord.value.toLowerCase())){
 
-      board[round].bordState = inputWord.value;
-      
-      return console.log(`${round} Try Again`)
+      board[round.value].bordState = inputWord.value;
+      words.value.push(...board[round.value].bordState.split(""))
+      round.value++
+      return console.log(`${round.value} Try Again`)
     }
     else {
-      return console.log(`${round} don't have this word`);
+      return console.log(`${round.value} don't have this word`);
     }
     
   }
   const checkAnswer = () => {
-    for(let inputIdx = 0;eleInput< inputWord.value.length; inputIdx++){
-      for(let randIdx = 0;eleRand< randomWord.length;randIdx++){
+    for(let inputIdx = 0; eleInput< inputWord.value.length; inputIdx++){
+      for(let randIdx = 0; eleRand< randomWord.length;randIdx++){
         if(inputWord.value[inputIdx]==randomWord[randIdx]){
           
         }
@@ -69,13 +71,20 @@
   
 </div>
 
-{{inputWord}}
+
 <div class="flex justify-center ">
   <div class="form-control">
     <input type="text" placeholder="ENTER YOUR WORD !!" style="text-transform:uppercase" class="input bg-base-200" maxlength="5" v-model="inputWord" @keyup.enter="checkInput">
   </div>
 </div>
-  
+
+<div class="bg-blue-400 text-blue-400 min-h-screen flex items-center justify-center">
+  <div class="grid grid-cols-5 gap-4">
+    <div class="bg-white p-5 rounded list-none uppercase" v-for="boards in words">
+    {{ boards }}
+    </div>
+  </div>
+</div>
 </template>
 
 <style>
