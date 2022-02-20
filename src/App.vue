866087f36ca5abf10f76ca347ea1157079b2efde
Panupong <panupong.n@mail.kmutt.ac.th>
2022-02-20 19:45:00 +0700

    checkWin/Lose

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,168 @@
 <script setup>
-  import {ref,reactive} from 'vue'
-  import {word} from './word.json'
-  
-  const evalueteStatus = ["present","correct","absent"]
+import { ref, reactive, computed } from 'vue';
+import { word } from './word.json';
 
-  const word1 = word.word1;
-  const word2 = word.word2;
+const evalueteStatus = {
+  PRESENT: 'present',
+  CORRECT: 'correct',
+  ABSENT: 'absent',
+};
 
-  const randomWord = word1[Math.floor(Math.random()*2315)]
-  const inputWord = ref('')
-  let round = 0
-  const boards = reactive([{
-    bordState : '',
-    evalution : []
-    },
-    {
-    bordState : '',
-    evalution : []
-    },
-    {
-    bordState : '',
-    evalution : []
-    },
-    {
-    bordState : '',
-    evalution : []
-    },
-    {
-    bordState : '',
-    evalution : []
-    },
-    {
-    bordState : '',
-    evalution : []
-    }])
+const word1 = word.word1;
+const word2 = word.word2;
 
-const checkWin = ref(false)
+const randomWord = word1[Math.floor(Math.random() * 2315)];
+const inputWord = ref('');
+const round = ref(0);
+const boards = reactive([
+  {
+    bordState: '',
+    evalution: [],
+  },
+  {
+    bordState: '',
+    evalution: [],
+  },
+  {
+    bordState: '',
+    evalution: [],
+  },
+  {
+    bordState: '',
+    evalution: [],
+  },
+  {
+    bordState: '',
+    evalution: [],
+  },
+  {
+    bordState: '',
+    evalution: [],
+  },
+]);
 
-  console.log(randomWord);
-  const checkInput = () => {
-    if(round==6){return console.log('You Lose');}
-    if(randomWord==inputWord.value.toLowerCase()){
-      checkWin.value = true;
-      return console.log(`round:${round} You WIN`);
+const checkWin = ref(false);
+const checkLose = ref(false);
+
+const words = computed(() => {
+  const wordList = [];
+  for (const wb of boards) {
+    if (wb.bordState !== '') {
+      wordList.push(...wb.bordState.split(''));
     }
-    if(word1.includes(inputWord.value.toLowerCase())||word2.includes(inputWord.value.toLowerCase())){
-
-      boards[round].bordState = inputWord.value;
-      round++
-      return console.log(`${round} Try Again`)
-    }
-    else {
-      return console.log(`${round} don't have this word`);
-    }
-    
   }
-  const checkAnswer = () => {
-    for(let inputIdx = 0;eleInput< inputWord.value.length; inputIdx++){
-      for(let randIdx = 0;eleRand< randomWord.length;randIdx++){
-        if(inputWord.value[inputIdx]==randomWord[randIdx]){
-          
+  return wordList;
+});
+
+const evalutes = computed(() => {
+  const evaluteList = [];
+  for (const eb of boards) {
+    if (eb.evalution !== '') {
+      evaluteList.push(...eb.evalution);
+    }
+  }
+  return evaluteList;
+});
+
+console.log(randomWord);
+
+const checkInput = () => {
+  if (randomWord == inputWord.value.toLowerCase()) {
+    setWord();
+    round.value++;
+    console.log('you win');
+    checkWin.value = true;
+  } else if (
+    word1.includes(inputWord.value.toLowerCase()) ||
+    word2.includes(inputWord.value.toLowerCase())
+  ) {
+    setWord();
+    round.value++;
+    console.log(`${round.value} Try Again`);
+  } else {
+    setWord();
+    console.log(`${round.value} don't have this word`);
+  }
+  if (round.value == 6) {
+    checkLose.value = true;
+  }
+};
+
+const checkAnswer = () => {
+  const evaList = [];
+  for (let inputIdx = 0; inputIdx < inputWord.value.length; inputIdx++) {
+    for (let randIdx = 0; randIdx < randomWord.length; randIdx++) {
+      if (inputWord.value[inputIdx] == randomWord[randIdx]) {
+        if (inputIdx == randIdx) {
+          evaList.push(evalueteStatus.CORRECT);
+        } else {
+          evaList.push(evalueteStatus.PRESENT);
         }
+        break;
       }
     }
+    if (evaList.length === inputIdx) {
+      evaList.push(evalueteStatus.ABSENT);
+    }
   }
+  return evaList;
+};
 
-const fillLettet = (letter: String) => {
-
-}
-
+const setWord = () => {
+  boards[round.value].bordState = inputWord.value;
+  boards[round.value].evalution = checkAnswer();
+};
 </script>
 
 <template>
-<div class="flex justify-center ">
-  
-</div>
+  <div class="flex justify-center"></div>
 
-{{inputWord}}
 
-<div class="flex justify-center ">
-  <div class="form-control">
-    <input type="text" placeholder="ENTER YOUR WORD !!" style="text-transform:uppercase" class="input bg-base-200" maxlength="5" v-model="inputWord" @keyup.enter="checkInput" :readonly="checkWin === true">
+  <div class="flex justify-center">
+    <div class="form-control">
+      <input
+        type="text"
+        placeholder="ENTER YOUR WORD !!"
+        :class="['input bg-base-200 uppercase', checkWin === true ? 'pointer-events-none' : '']"
+        maxlength="5"
+        v-model="inputWord"
+        @keyup.enter="checkInput"
+        :readonly="checkWin === true || checkLose === true"
+      />
+    </div>
   </div>
-</div>
-<div class ="m-5">
-   <p v-show="checkWin === true">You WIN!</p> 
-   </div>
+  <div class="m-5 text-green-600">
+    <p v-show="checkWin === true">You WIN!</p>
+  </div>
 
-<div class = "grid max-w-xs grid-cols-5 gap-5 mx-auto mb-1">
-   <div 
-   v-for="i in 5"
-   :key="i"
-   class = "grid grid-cols-5 gap-5 p-5  border-2 border-gray-900">
+  <div class="m-5 text-red-600">
+    <p v-show="checkLose === true">You LOSE!</p>
+  </div>
+<div v-show = "checkWin == true || checkLose == true" class="uppercase m-5 bg-"> 
+  <p>{{randomWord}}</p>
+  </div>
+  <div
+    class="bg-blue-400 text-blue-400 min-h-screen flex items-center justify-center"
+  >
 
-
-    
-     
    
-     
-
-
-   </div>
-</div>
-
-
+ 
+    <div class="grid grid-cols-5 gap-4">
+      
+      <div
+        class="p-5 rounded list-none uppercase"
+        :class="{
+          'bg-white ': evalutes[index] == evalueteStatus.ABSENT,
+          'bg-green-300 text-white': evalutes[index] == evalueteStatus.CORRECT,
+          'bg-amber-300 text-white': evalutes[index] == evalueteStatus.PRESENT,
+        }"
+        v-for="(boards, index) in words"
+      >
+        {{ boards }}
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <style>
